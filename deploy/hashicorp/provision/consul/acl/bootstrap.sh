b07e4b0b48a2c1e08ef203d5bc883964ad6e54cc
Cristian Ramirez <Cristian.Ramirez@shutterfly.com>
2019-10-21 16:57:05 -0500

    adding consul acl policies

---
+++ deploy/hashicorp/provision/consul/acl/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "curl -s --request PUT http://172.20.20.11:8500/v1/acl/bootstrap"
+response=`curl -s --request PUT http://172.20.20.11:8500/v1/acl/bootstrap`
+echo $response
+
+rootToken=`echo $response | jq .SecretID | sed s/\"//g`
+
+echo "CONSUL_HTTP_TOKEN=$rootToken consul acl set-agent-token default $rootToken"
+CONSUL_HTTP_TOKEN=$rootToken consul acl set-agent-token default $rootToken
+
+echo "consul kv put cluster/consul/rootToken $rootToken"
+consul kv put cluster/consul/rootToken $rootToken
+
+
