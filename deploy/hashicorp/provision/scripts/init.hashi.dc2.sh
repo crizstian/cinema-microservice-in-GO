3cffe06b2024657f06e875f05a70fd566b4877a1
Cristian Ramirez <Cristian.Ramirez@shutterfly.com>
2019-10-21 16:59:47 -0500

    removing vault config for dc2

--- deploy/hashicorp/provision/scripts/init.hashi.dc2.sh
+++ deploy/hashicorp/provision/scripts/init.hashi.dc2.sh
@@ -10,15 +10,15 @@ sudo cp /vagrant/provision/nomad/dc2/* /var/nomad/config/
 sudo cp /vagrant/provision/nomad/system/nomad.service /etc/systemd/system/nomad.service
 sudo chmod +x /vagrant/provision/nomad/system/*.sh
 
-sudo cp /vagrant/provision/vault/dc1/* /var/vault/config/
-sudo cp /vagrant/provision/vault/system/vault.service /etc/systemd/system/vault.service
-sudo chmod +x /vagrant/provision/vault/system/*.sh
+# sudo cp /vagrant/provision/vault/dc1/* /var/vault/config/
+# sudo cp /vagrant/provision/vault/system/vault.service /etc/systemd/system/vault.service
+# sudo chmod +x /vagrant/provision/vault/system/*.sh
 
 sudo cp /vagrant/provision/docker-config/dc2/daemon.json /etc/docker/daemon.json
 
 sudo systemctl daemon-reload
 sudo service consul restart
-sudo service vault restart
+# sudo service vault restart
 sudo service nomad restart
 sudo service docker restart
 
