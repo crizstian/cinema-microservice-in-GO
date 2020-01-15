0b0531955aaeaa92bbaa6b27e835ed20bc15207f
Cristian Ramirez <cristianramirez@MacBook-Pro-de-Cristian.local>
2020-01-15 14:13:13 -0600

    adding silence mode

--- deploy/hashicorp/provision/vault/dr/dc2.sh
+++
@@ -1,7 +0,0 @@
-. /etc/environment
-
-echo "consul kv get -http-addr=172.20.20.11:8500 cluster/vault/secondary/token"
-token=`consul kv get -http-addr=172.20.20.11:8500 cluster/vault/secondary/token`
-
-echo "vault write sys/replication/dr/secondary/enable token=$token"
-vault write sys/replication/dr/secondary/enable token=$token ca_file=/var/vault/config/ca.crt.pem
